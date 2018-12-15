48fb5b830ccf87ff9d8503305b787a820628dd0d
Solomon Xie <solomonxiewise@gmail.com>
2018-12-15 17:27:48 +0800

    finished basic relationships between tables [Tracks]; Tested with data basic insertion and basic query

--- sqlschemas/ORM/CREATE_TABLES.py
+++ sqlschemas/ORM/CREATE_TABLES.py
@@ -59,44 +59,34 @@ class Host(Base):
 class Track(Base):
     __tablename__ = 'ref_Tracks'
 
-
     tid = Column('tid', Integer, primary_key=True)
-    title = Column('title', String)
-    #tid_spt = Column('tid_spt', Integer, ForeignKey('spotify_Tracks.tid_spt'))
-    #tid_mbz = Column('tid_mbz', Integer, ForeignKey('musicbrainz_Tracks.tid_mbz'))
+    tid_spt = Column('tid_spt', Integer, ForeignKey('spotify_Tracks.tid'))
+    tid_mbz = Column('tid_mbz', Integer, ForeignKey('musicbrainz_Tracks.tid'))
     #tids_fs = Column('tids_fs', Sequence)
 
     users = relationship('User', secondary=user_tracks, back_populates='tracks')
 
 
+class Track_SPT(Base):
+    """
+    Independent table, without any setup to relate User tables
+    Manually search by id if needed.
+    """
+    __tablename__ = 'spotify_Tracks'
+
+    tid = Column('tid', Integer, primary_key=True)
+    title = Column('title', String)
+
+
 class Track_MBZ(Base):
+    """
+    Independent table, without any setup to relate User tables
+    Manually search by id if needed.
+    """
     __tablename__ = 'musicbrainz_Tracks'
 
-    tid = Column('tid_mbz', Integer, primary_key=True)
+    tid = Column('tid', Integer, primary_key=True)
     title = Column('title', Integer)
-
-
-#------ Start of Data Insersions ---------
-h1 = Host(name='Spotify')
-h2 = Host(name='MusicBrainz')
-h3 = Host(name='iTunes')
-
-t1 = Track(title='139')
-t2 = Track(title='Hey Jdue')
-t3 = Track(title='Now is not a good time')
-
-u1 = User(name='Jason', hosts=[h1,h2])
-u1.tracks += [t1,t2,t3]
-
-u2 = User(name='David', hosts=[h1,h3])
-u3 = User(name='Sol', hosts=[h1,h2,h3])
-
-#------- End of Data Insersions ---------
-
-
-
-#------- Start of Data Browsing ---------
-#------- End of Data Browsing ---------
 
 
 
@@ -112,19 +102,59 @@ Base.metadata.drop_all(engine)
 
 # Let new Schemas take effect
 Base.metadata.create_all(bind=engine)
-session = sessionmaker(bind=engine)()
+session = sessionmaker(bind=engine, autoflush=False)()
 
-session.add(u1)
-session.add(u2)
-session.add(u3)
+    # Start of Data Insersions --------{
 
-session.add(h1)
-session.add(h2)
-session.add(h3)
+h1 = Host(name='Spotify')
+h2 = Host(name='MusicBrainz')
+h3 = Host(name='iTunes')
+session.add_all([h1, h2, h3])
+session.flush()  # Flush data for Dynamic Fileds to get their IDs
+
+# Add source tracks in Spotify's Library
+src1_1 = Track_SPT(title='139')
+src1_2 = Track_SPT(title='Hey Jdue')
+src1_3 = Track_SPT(title='Now is not a good time')
+session.add_all([src1_1, src1_2, src1_3])
+
+# Add source tracks in MusicBrainz's Library
+src2_1 = Track_MBZ(title='139')
+src2_2 = Track_MBZ(title='Hey Jude')
+src2_3 = Track_MBZ(title='Now is not a good time')
+session.add_all([src2_1, src2_2, src2_3])
+session.flush()  # Generate data for Dynamic fileds(primary key) to get values
+
+# Add User tracks with connection to multiple Source Libraries
+t1 = Track(tid_spt=src1_1.tid, tid_mbz=src2_1.tid)
+t2 = Track(tid_spt=src1_2.tid, tid_mbz=src2_2.tid)
+t3 = Track(tid_spt=src1_3.tid, tid_mbz=src2_3.tid)
+session.add_all([t1, t2, t3])
+session.flush()  # Generate data for Dynamic fileds(primary key) to get values
+
+u1 = User(name='Jason', hosts=[h1,h2], tracks=[t1,t2,t3])
+u2 = User(name='David', hosts=[h1,h3], tracks=[t1,t2])
+u3 = User(name='Sol', hosts=[h1,h2,h3], tracks=[t1,t3])
+session.add_all([u1,u2,u3])
+session.flush()  # Generate data for Dynamic fileds(primary key) to get values
+
+    # }------- End of Data Insersions
+
 
 session.commit()
 session.close()
 #------- End of Data Submitting ---------
+
+
+# Start of Data Browsing ---------{
+u = session.query(User).first()
+print(u.name)
+for t in u.tracks:
+    if t.tid_spt:
+        t_spt = session.query(Track_SPT).filter(Track_SPT.tid == t.tid_spt).first()
+        if t_spt:
+            print('Track:%s'% t_spt.title)
+# }------- End of Data Browsing
 
 
 print('[  OK  ]')
