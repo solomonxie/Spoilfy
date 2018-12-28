e1485b03269b2b2766f00f90e48a961df2474f26
Solomon Xie <solomonxiewise@gmail.com>
2018-12-28 16:50:26 +0800

    update

---
+++ scratch/tagger/spotifytag.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# MAINTAINER: Solomon Xie <solomonxiewise@gmail.com>
+#
+# DEPENDENCIES:
+
+import sys
+sys.path.append('../../Spoilfy')
+print( '[PATH] \n\t{}\npath at :\n\t{}\n\n'.format(__file__, sys.path) )
+
+
+# from Spoilfy.orm.spotify import *
+from orm import SpotifyAccount
+
+class Spotify2MusicBrainz:
+    pass
+
+
+
+
+
+
+# ==============================================================
+# >>>>>>>>>>>>>>>>>>>>>>[    TEST     ] >>>>>>>>>>>>>>>>>>>>>>>>
+# ==============================================================
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+print('[  OK  ] __IMPORTED__: {}'.format(__name__))
