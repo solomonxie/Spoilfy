9ba171dc5356b1ca33cdd50ba068077f0c64ed05
Solomon Xie <solomonxiewise@gmail.com>
2018-12-31 02:50:41 +0800

    <feat> implemented Musicbrainz JSON API

--- Spoilfy/orm/musicbrainz.py
+++ Spoilfy/orm/musicbrainz.py
@@ -25,6 +25,7 @@ Explain:
 """
 
 
+
 class MusicbrainzResource(Resource):
     __abstract__ = True
 
@@ -32,55 +33,35 @@ class MusicbrainzResource(Resource):
     external_urls = Column('external_urls', String)
 
 
-
-class MusicbrainzAccount(Resource):
-    """ [ Store User Accounts with Musicbrainz ]
-        Information might involve with Authentication / Password.
-    """
-    __tablename__ = 'mbz_Accounts'
-
-    @classmethod
-    def add(cls, jsondata):
-        user = cls(
-            uri = jsondata['uri'],
-            id = jsondata['id'],
-            type = jsondata['type'],
-            provider = 'musicbrainz',
-            name = jsondata['name']
-        )
-        cls.session.merge(user)
-        # cls.session.commit()
-        print('[  OK  ] Inserted Musicbrainz User: {}.'.format(user.name))
-
-        return user
-
-
-
 class MusicbrainzTrack(Resource):
     """ [ Track resources in Musicbrainz ]
     """
     __tablename__ = 'mbz_Tracks'
 
-    atids = Column('atids', String)
-    abids = Column('abids', String)
+    album_uris = Column('album_uris', String)
+    artist_uris = Column('artist_uri', String)
 
-    score = Column('score', Integer)
-    length = Column('length', Integer)
+    length = Column('length', String)
+    score = Column('score', String)
 
-    @classmethod
-    def add(cls, jsondata):
-        j = jsondata['recording']
-        item = cls(
-            uri = j['uri'],
-            name = j['title'],
-            id = j['id'],
+    def __init__(self, data):
+        d = data
+        super().__init__(
+            uri = 'musicbrainz:track:{}'.format( d.get('id') ),
+            id = d.get('id'),
             type = 'track',
             provider = 'musicbrainz',
+            name = d.get('title'),
+            album_uris = 'musicbrainz:album:{}'.format(','.join(
+                [ r.get('id') for r in d.get('releases',{}) ]
+            )),
+            artist_uris = 'musicbrainz:artist:{}'.format(','.join(
+                [a.get('artist',{}).get('id') for a in d.get('artist-credit',{})]
+            )),
+            score = d.get('score'),
+            length = d.get('length')
         )
-        cls.session.merge( item )   #Merge existing data
-        #cls.session.commit()  #-> Better to commit after multiple inserts
-
-        return item
+        self.session.merge( self )
 
 
 class MusicbrainzAlbum(Resource):
@@ -88,47 +69,49 @@ class MusicbrainzAlbum(Resource):
     """
     __tablename__ = 'mbz_Albums'
 
-    atids = Column('artist_ids', String)
-    tids = Column('track_ids', String)
+    artist_uris = Column('artist_uris', String)
+    track_uris = Column('track_uris', String)
 
-    release_date = Column('release_date', String)
-    release_date_precision = Column('release_date_precision', String)
-    total_tracks = Column('total_tracks', Integer)
-    lable = Column('lable', String)
-    popularity = Column('popularity', Integer)
-    copyrights = Column('copyrights', String)
-    album_type = Column('album_type', String)
-    external_ids = Column('external_ids', String)
-    href = Column('href', String)
-    external_urls = Column('external_urls', String)
+    score = Column('score', Integer)
+    track_count = Column('track_count', Integer)
+    status = Column('status', String)
+    country = Column('country', String)
+    date = Column('date', String)
+    release_events = Column('release_events', String)
+    release_group = Column('release_group', String)
+    barcode = Column('barcode', String)
+    count = Column('count', Integer)
+    label_info = Column('label_info', String)
+    media = Column('media', String)
+    text_representation = Column('text_representation', String)
 
 
-
-    @classmethod
-    def add(cls, jsondata):
-        d = jsondata['album']
-        item = cls(
-            uri = d['uri'],
-            name = d['name'],
-            id = d['id'],
-            type = d['type'],
+    def __init__(self, jsondata):
+        d = jsondata
+        super().__init__(
+            uri = 'musicbrainz:album:{}'.format( d.get('id') ),
+            name = d.get('title'),
+            id = d.get('id'),
+            type = 'album',
             provider = 'musicbrainz',
-            atids = ','.join([ a['id'] for a in d['artists'] ]),
-            tids = ','.join([ a['id'] for a in d['tracks']['items'] ]),
-            album_type = d['album_type'],
-            release_date = d['release_date'],
-            release_date_precision = d['release_date_precision'],
-            total_tracks = d['total_tracks'],
-            lable = d['label'],
-            popularity = d['popularity'],
-            copyrights = str(d['copyrights']),
-            href = d['href'],
-            external_urls = str(d['external_urls']),
-            external_ids = str(d['external_ids'])
+            track_uris = None,
+            artist_uris = 'musicbrainz:artist:{}'.format(','.join(
+                [a.get('artist',{}).get('id') for a in d.get('artist-credit',{})]
+            )),
+            score = d.get('score'),
+            track_count = d.get('track-count'),
+            status = d.get('status'),
+            country = d.get('country'),
+            date = d.get('date'),
+            release_events = str(d.get('release-events')),
+            release_group = str(d.get('release-group')),
+            barcode = d.get('barcode'),
+            count = d.get('count'),
+            label_info = str(d.get('label-info')),
+            media = str(d.get('media')),
+            text_representation = str(d.get('text-representation')),
         )
-        cls.session.merge( item )
-        #cls.session.commit()  #-> Better to commit after multiple inserts
-        return item
+        self.session.merge( self )
 
 
 
@@ -137,107 +120,39 @@ class MusicbrainzArtist(Resource):
     """
     __tablename__ = 'mbz_Artists'
 
-    genres = Column('genres', String)
-    followers = Column('followers', Integer)
-    popularity = Column('popularity', Integer)
-    href = Column('href', String)
-    external_urls = Column('external_urls', String)
+    artist_type = Column('artist_type', String)
+    artist_type_id = Column('artist_type_id', String)
+    score = Column('score', String)
+    sort_name = Column('sort_name', String)
+    aliases = Column('aliases', String)
+    area = Column('area', String)
+    begin_area = Column('begin_area', String)
+    country = Column('country', String)
+    disambiguation = Column('disambiguation', String)
+    lifespan = Column('lifespan', String)
+    tags = Column('tags', String)
 
-
-    @classmethod
-    def add(cls, jsondata):
+    def __init__(self, jsondata):
         d = jsondata
-        item = cls(
-            uri = d['uri'],
-            name = d['name'],
-            id = d['id'],
-            type = d['type'],
+        super().__init__(
+            uri = 'musicbrainz:artist:{}'.format( d.get('id') ),
+            name = d.get('name'),
+            id = d.get('id'),
+            type = 'artist',
             provider = 'musicbrainz',
-            genres = str(d['genres']),
-            followers = d['followers']['total'],
-            popularity = d['popularity'],
-            href = d['href'],
-            external_urls = str(d['external_urls'])
+            artist_type = d.get('type'),
+            artist_type_id = d.get('type-id'),
+            score = d.get('score'),
+            sort_name = d.get('sort-name'),
+            aliases = str(d.get('aliases')),
+            area = str(d.get('area')),
+            begin_area = str(d.get('begin-area')),
+            country = d.get('country'),
+            disambiguation = d.get('disambiguation'),
+            lifespan = str(d.get('lifespan')),
+            tags = str(d.get('tags')),
         )
-        cls.session.merge( item )   #Merge existing data
-        #cls.session.commit()  #-> Better to commit after multiple inserts
-        return item
-
-
-
-class MusicbrainzPlaylist(Resource):
-    """ [ Playlist resources in Musicbrainz ]
-    """
-    __tablename__ = 'mbz_Playlists'
-
-    owner_id = Column('owner_id', String)
-    snapshot_id = Column('snapshot_id', String)
-    tids = Column('track_ids', String)
-
-    total_tracks = Column('total_tracks', Integer)
-    followers = Column('followers', Integer)
-    collaborative = Column('collaborative', Boolean)
-    description = Column('description', String)
-    public = Column('public', Boolean)
-    images = Column('images', String)
-    href = Column('href', String)
-    external_urls = Column('external_urls', String)
-
-    @classmethod
-    def add(cls, jsondata):
-        j = jsondata
-        u = j['uri'].split(':')
-        item = cls(
-            uri = '{}:{}:{}'.format(u[0],u[3],u[4]),
-            name = j['name'],
-            id = j['id'],
-            type = j['type'],
-            provider = 'musicbrainz',
-            owner_id = j['owner']['id'],
-            snapshot_id = j['snapshot_id'],
-            total_tracks = j['tracks']['total'],
-            public = j['public'],
-            collaborative = j['collaborative'],
-            images = str(j['images']),
-            href = j['href'],
-            external_urls = j['external_urls']['musicbrainz'],
-            #-> [Keys below are to be retrieved dynamically]:
-            #tids = str(j['tracks']['href']),
-            #followers = j['followers']['total'],
-            #description = j['description'],
-        )
-        cls.session.merge( item )   #Merge existing data
-        #-> Temporary: For test only to solve repeated ID issue.
-        cls.session.commit()  #-> Better to commit after multiple inserts
-        return item
-
-    @classmethod
-    def add_resources(cls, items):
-        """[ Add Resources ]
-        :param session: sqlalchemy SESSION binded to DB.
-        :param LIST items: must be iteratable.
-        :return: inserted resource objects.
-        """
-        all = []
-        for item in items:
-            data = cls.get_playlist_tracks(item)
-            obj = cls.add(data)
-            all.append( obj )
-
-        cls.session.commit()
-        print('[  OK  ] Inserted {} items to [{}].'.format(
-            len(all), cls.__tablename__
-        ))
-
-        return all
-
-    @classmethod
-    def get_playlist_tracks(cls, item):
-        """ [ Get sub item's data through Web API  ]
-            This should retrive WebAPI accordingly
-            This is to impelemented by children class.
-        """
-        return item
+        self.session.merge( self )   #Merge existing data
 
 
 
--- Spoilfy/orm/test_musicbrainz.py
+++ Spoilfy/orm/test_musicbrainz.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 
 from common import engine, Resource, Reference
 from user import UserAccount, UserResource
-from musicbrainz import MusicbrainzAccount, MusicbrainzTrack, MusicbrainzAlbum, MusicbrainzArtist, MusicbrainzPlaylist
+from musicbrainz import MusicbrainzTrack, MusicbrainzAlbum, MusicbrainzArtist
 
 session = sessionmaker(bind=engine, autoflush=False)()
 
@@ -22,24 +22,60 @@ session = sessionmaker(bind=engine, autoflush=False)()
 # >>>>>>>>>>>>>>>>>>>>>>[    TEST     ] >>>>>>>>>>>>>>>>>>>>>>>>
 # ==============================================================
 
-def test_MusicbrainzAccount():
-    pass
-
-
 def test_MusicbrainzTrack():
-    pass
+    try:
+        MusicbrainzTrack.__table__.drop(engine)
+        MusicbrainzTrack.metadata.create_all(bind=engine)
+    except Exception as e:
+        print('Error on dropping Musicbrainz table.')
+
+
+    # Add a track
+    path = '../../scratch/sqlschemas/musicbrainz/jsondumps/search_recording.json'
+    with open(path, 'r') as f:
+        jsondata = json.loads( f.read() )
+        # Create items
+        items = MusicbrainzTrack.add_resources(jsondata['recordings'])
+        # Add reference
+        Reference.add_resources(items)
 
 
 def test_MusicbrainzAlbum():
-    pass
+    try:
+        MusicbrainzAlbum.__table__.drop(engine)
+        MusicbrainzAlbum.metadata.create_all(bind=engine)
+    except Exception as e:
+        print('Error on dropping Musicbrainz table.')
+
+
+    # Add a track
+    path = '../../scratch/sqlschemas/musicbrainz/jsondumps/search_release.json'
+    with open(path, 'r') as f:
+        jsondata = json.loads( f.read() )
+        # Create items
+        items = MusicbrainzAlbum.add_resources(jsondata.get('releases',{}))
+        # Add reference
+        # Reference.add_resources(items)
 
 
 def test_MusicbrainzArtist():
-    pass
+    try:
+        MusicbrainzArtist.__table__.drop(engine)
+        MusicbrainzArtist.metadata.create_all(bind=engine)
+    except Exception as e:
+        print('Error on dropping Musicbrainz table.')
 
 
-def test_MusicbrainzPlaylist():
-    pass
+    # Add a track
+    path = '../../scratch/sqlschemas/musicbrainz/jsondumps/search_artists.json'
+    with open(path, 'r') as f:
+        jsondata = json.loads( f.read() )
+        # Create items
+        items = MusicbrainzArtist.add_resources(jsondata.get('artists',{}))
+        # Add reference
+        # Reference.add_resources(items)
+
+
 
 
 def test_query_track():
@@ -61,15 +97,12 @@ def test_query_playlist():
 
 if __name__ == '__main__':
     #=> Insert data
-    test_MusicbrainzAccount()
     test_MusicbrainzTrack()
     test_MusicbrainzAlbum()
     test_MusicbrainzArtist()
-    test_MusicbrainzPlaylist()
 
     #=> Query
-    test_query_track()
-    test_query_album()
-    test_query_artist()
-    test_query_playlist()
+    # test_query_track()
+    # test_query_album()
+    # test_query_artist()
 
