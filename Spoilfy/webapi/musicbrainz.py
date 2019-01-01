bda2192da362640263caf03d837934afcc8963e1
Solomon Xie <solomonxiewise@gmail.com>
2019-01-02 03:23:19 +0800

    <refactor> musicbbrainz API as functions instead of class

--- Spoilfy/webapi/musicbrainz.py
+++ Spoilfy/webapi/musicbrainz.py
@@ -9,45 +9,41 @@ import xmltodict
 import requests
 
 
-class MusicbrainzAPI:
-    """ [  ]
+ROOT = 'http://musicbrainz.org/ws/2'
+APP = {'User-Agent':'Spoilfy/0.0.1 (solomonxiewise@gmail)'}
 
+def _get(url, params={}):
+    r = requests.get(url, headers=APP, params=params)
+    print( '[FETCHING]', params, r.url )
+    return xmltodict.parse(r.content)
+
+def _qs(query):
+    """[ Get formated query string ]
+        example: 'name:bigbang AND country:Norway'
     """
-    ROOT = 'http://musicbrainz.org/ws/2'
-    APP = {'User-Agent':'Spoilfy/0.0.1 (solomonxiewise@gmail)'}
+    return ' AND '.join(
+        ['{0}:{1}'.format(k,v) for k,v in query.items()]
+    )
 
-    def _get(self, url, params={}):
-        r = requests.get(url, headers=self.APP, params=params)
-        print( '[FETCHING]', params, r.url )
-        return xmltodict.parse(r.content)
+# [Tracks]
+def search_tracks(**query):
+    return _get(ROOT+'/recording',
+        params={'query':_qs(query)}
+    )
 
-    def _qs(self, query):
-        """[ Get formated query string ]
-            example: 'name:bigbang AND country:Norway'
-        """
-        return ' AND '.join(
-            ['{0}:{1}'.format(k,v) for k,v in query.items()]
-        )
+# [Albums]
+def search_albums(**query):
+    return _get(ROOT+'/release',
+        params={'query':_qs(query)}
+    )
 
-    # [Tracks]
-    def search_tracks(self, **query):
-        return self._get(self.ROOT+'/recording',
-            params={'query':self._qs(query)}
-        )
-
-    # [Albums]
-    def search_albums(self, **query):
-        return self._get(self.ROOT+'/release',
-            params={'query':self._qs(query)}
-        )
-
-    # [Artists]
-    def search_artists(self, **query):
-        return self._get(self.ROOT+'/artist',
-            params={'query':self._qs(query)}
-        )
+# [Artists]
+def search_artists(**query):
+    return _get(ROOT+'/artist',
+        params={'query':_qs(query)}
+    )
 
 
 
 
-print('[ OK ] __IMPORTED__: {}'.format(__name__))
+print('[  OK  ] __IMPORTED__: {}'.format(__name__))
--- Spoilfy/webapi/test_musicbrainz.py
+++ Spoilfy/webapi/test_musicbrainz.py
@@ -7,7 +7,7 @@
 import json
 import unittest
 
-from musicbrainz import MusicbrainzAPI
+import musicbrainz as mbz
 
 
 
@@ -18,22 +18,21 @@ from musicbrainz import MusicbrainzAPI
 
 
 def main():
-    mba = MusicbrainzAPI()
 
     # Search Tracks
-    results = mba.search_tracks(name='bigbang', country='NO')
+    results = mbz.search_tracks(name='bigbang', country='NO')
     for obj in results['metadata']['recording-list']['recording']:
         print( '\t[TRACK]:', obj['title'], obj['length'], obj['@id'] )
     #obj = results['metadata']['artist-list']['artist'][0]
 
     # Search Albums
-    results = mba.search_albums(name='edendale', country='NO')
+    results = mbz.search_albums(name='edendale', country='NO')
     #obj = results['metadata']['artist-list']['artist'][0]
     for obj in results['metadata']['release-list']['release']:
         print( '\t[ALBUM]:', obj['title'], obj['date'], obj['@id'] )
 
     # Search Artists
-    results = mba.search_artists(name='bigbang', country='NO')
+    results = mbz.search_artists(name='bigbang', country='NO')
     #obj = results['metadata']['artist-list']['artist'][0]
     for obj in results['metadata']['artist-list']['artist']:
         print( '\t[ARTIST]:', obj['country'], obj['name'], obj['@id'] )
